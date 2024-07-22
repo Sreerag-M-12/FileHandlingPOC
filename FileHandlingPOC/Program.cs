ccbb02be9947b87ee9d0805f06eada325c2233b3
Sreerag Rajesh Babu <Sreerag.Babu@asl.mum.sst>
2024-07-22 20:05:18 +0530

    innit

---
+++ FileHandlingPOC/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace FileHandlingPOC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string readTextFile = @"C:\Users\sreerag.babu\source\repos\FileHandlingPOC\Assets\ReadFile.txt";
+
+            Console.WriteLine("Reading File using File.ReadAllText()");
+
+            // To read the entire file at once 
+            if (File.Exists(readTextFile))
+            {
+                string str = File.ReadAllText(readTextFile);
+                Console.WriteLine(str);
+            }
+            Console.WriteLine();
+
+            string writeTextFile = @"C:\Users\sreerag.babu\source\repos\FileHandlingPOC\Assets\WriteFile.txt";
+            string text = "This is written on C#";
+            File.WriteAllText(writeTextFile, text);
+
+            string text2 = "This is text to be appended";
+            File.AppendAllText(writeTextFile, text2);
+
+            Console.WriteLine(File.ReadAllText(writeTextFile));
+
+
+            //read write html file
+            string writeHtmlFile = @"C:\Users\sreerag.babu\source\repos\FileHandlingPOC\Assets\writeName.html";
+            using (StreamWriter sw = new StreamWriter(writeHtmlFile)) 
+            {
+                    string textHtml = "<html lang=\"en\">\r\n" +
+                    "<head>\r\n   " +
+                    " <title>WriteHtmlFile</title>\r\n" +
+                    "</head>\r\n<body>\r\n   " +
+                    " <h1>Hello World</h1>\r\n    " +
+                    "<b>Bold heading</b>\r\n    " +
+                    "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. " +
+                    "Animi consectetur perferendis excepturi quos similique et libero " +
+                    "consequatur consequuntur molestiae possimus tenetur, " +
+                    "eum recusandae non! Quisquam, officiis. Ducimus asperiores quae sint.</p>\r\n" +
+                    "</body>\r\n" +
+                    "</html>";
+                    sw.WriteLine(textHtml, true);
+            }
+
+
+
+            string readHtml = File.ReadAllText(writeHtmlFile);
+            Console.WriteLine("HTML file has following content");
+            Console.WriteLine(readHtml);
+
+
+        }
+    }
+}
---
+++ SaveUserNameFile/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace SaveUserNameFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a username:");
+            string username = Console.ReadLine();
+
+            if (!string.IsNullOrWhiteSpace(username))
+            {
+                SaveUsername(username);
+                Console.WriteLine($"Username '{username}' saved successfully.");
+            }
+            else
+                Console.WriteLine("Username cannot be empty.");
+        }
+
+
+        static void SaveUsername(string username)
+        {
+            string path = @"C:\Users\sreerag.babu\source\repos\SaveUserNameFile\Views\Username.txt";
+
+            string[] existingUsernames = File.ReadAllText(path).Split(',');
+
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+               
+                foreach (string existingUsername in existingUsernames)
+                {
+                    if (!string.IsNullOrWhiteSpace(existingUsername))
+                    {
+                        sw.Write(existingUsername + ",");
+                    }
+                }
+
+                // Write the new username at the end
+                sw.Write(username + ",");
+            }
+        }
+    }
+}
